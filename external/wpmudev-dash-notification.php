433d6dd0b64ba4bdfcfe6e1ac07d617b7526c82e
barry <barry@f53fe46e-8c02-459b-9fdd-93e75771b278>
2013-02-13 19:33:18 +0000

    updated dash notification
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/plugins/domain-mapping@8540 f53fe46e-8c02-459b-9fdd-93e75771b278

--- external/wpmudev-dash-notification.php
+++ external/wpmudev-dash-notification.php
@@ -3,13 +3,13 @@
 /* -------------------- WPMU DEV Dashboard Notice -------------------- */
 if ( !class_exists('WPMUDEV_Dashboard_Notice') ) {
 	class WPMUDEV_Dashboard_Notice {
-
+		
 		var $version = '2.0';
-
+		
 		function WPMUDEV_Dashboard_Notice() {
-			add_action( 'plugins_loaded', array( &$this, 'init' ) );
+			add_action( 'plugins_loaded', array( &$this, 'init' ) ); 
 		}
-
+		
 		function init() {
 			if ( !class_exists( 'WPMUDEV_Update_Notifications' ) && current_user_can( 'install_plugins' ) && is_admin() ) {
 				remove_action( 'admin_notices', 'wdp_un_check', 5 );
@@ -22,11 +22,11 @@ if ( !class_exists('WPMUDEV_Dashboard_Notice') ) {
 				}
 			}
 		}
-
+		
 		function filter_plugin_info($res, $action, $args) {
 			global $wp_version;
 			$cur_wp_version = preg_replace('/-.*$/', '', $wp_version);
-
+		
 			if ( $action == 'plugin_information' && strpos($args->slug, 'install_wpmudev_dash') !== false ) {
 				$res = new stdClass;
 				$res->name = 'WPMU DEV Dashboard';
@@ -36,31 +36,31 @@ if ( !class_exists('WPMUDEV_Dashboard_Notice') ) {
 				$res->homepage = 'http://premium.wpmudev.org/project/wpmu-dev-dashboard/';
 				$res->download_link = "http://premium.wpmudev.org/wdp-un.php?action=install_wpmudev_dash";
 				$res->tested = $cur_wp_version;
-
+				
 				return $res;
 			}
-
+	
 			return false;
 		}
-
+	
 		function auto_install_url() {
 			$function = is_multisite() ? 'network_admin_url' : 'admin_url';
 			return wp_nonce_url($function("update.php?action=install-plugin&plugin=install_wpmudev_dash"), "install-plugin_install_wpmudev_dash");
 		}
-
+		
 		function activate_url() {
 			$function = is_multisite() ? 'network_admin_url' : 'admin_url';
 			return wp_nonce_url($function('plugins.php?action=activate&plugin=wpmudev-updates%2Fupdate-notifications.php'), 'activate-plugin_wpmudev-updates/update-notifications.php');
 		}
-
+		
 		function install_notice() {
-			echo '<div class="error fade"><p>' . sprintf(__('Easily get updates, support, and one-click WPMU DEV plugin/theme installations right from in your dashboard - <strong><a href="%s" title="Install Now &raquo;">install the free WPMU DEV Dashboard plugin</a></strong>. &nbsp;&nbsp;&nbsp;<small><a href="http://premium.wpmudev.org/wpmu-dev/update-notifications-plugin-information/">(find out more)</a></small>', 'wpmudev'), $this->auto_install_url()) . '</a></p></div>';
+			echo '<div class="error fade"><p>' . sprintf(__('Easily get updates, support, and one-click WPMU DEV plugin/theme installations right from in your dashboard - <strong><a href="%s" title="Install Now &raquo;">install the free WPMU DEV Dashboard plugin</a></strong>. &nbsp;&nbsp;&nbsp;<small><a href="http://premium.wpmudev.org/wpmu-dev/update-notifications-plugin-information/">(find out more)</a></small>', 'wpmudev'), $this->auto_install_url()) . '</p></div>';
 		}
-
+		
 		function activate_notice() {
 			echo '<div class="updated fade"><p>' . sprintf(__('Updates, Support, Premium Plugins, Community - <strong><a href="%s" title="Activate Now &raquo;">activate the WPMU DEV Dashboard plugin now</a></strong>.', 'wpmudev'), $this->activate_url()) . '</a></p></div>';
 		}
-
+	
 	}
 	new WPMUDEV_Dashboard_Notice();
 }
