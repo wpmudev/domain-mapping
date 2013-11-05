d70c18451b3e31d65307a9c2c515bd2e65a20007
eugene <eugene@f53fe46e-8c02-459b-9fdd-93e75771b278>
2013-11-05 08:48:25 +0000

    The plugin version has been changed to 4.0.4
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/plugins/domain-mapping@11266 f53fe46e-8c02-459b-9fdd-93e75771b278

--- domain-mapping.php
+++ domain-mapping.php
@@ -3,7 +3,7 @@
 Plugin Name: Domain Mapping plugin
 Plugin URI: http://premium.wpmudev.org/project/domain-mapping
 Description: A domain mapping plugin that can handle sub-directory installs and global logins
-Version: 4.0.3
+Version: 4.0.4
 Author: Incsub
 Author URI: http://premium.wpmudev.org
 WDP ID: 99
