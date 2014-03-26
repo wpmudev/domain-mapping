9f60f267d812018b752e2bde4cbdd63868d75ae3
aaron <aaron@f53fe46e-8c02-459b-9fdd-93e75771b278>
2014-03-26 15:44:25 +0000

    Allow unfiltered html in new custom message
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/plugins/domain-mapping@13428 f53fe46e-8c02-459b-9fdd-93e75771b278

--- classes/Domainmap/Module/Pages.php
+++ classes/Domainmap/Module/Pages.php
@@ -202,7 +202,7 @@ class Domainmap_Module_Pages extends Domainmap_Module {
 			$options['map_logindomain'] = filter_input( INPUT_POST, 'map_logindomain' );
 			$options['map_crossautologin'] = filter_input( INPUT_POST, 'map_crossautologin', FILTER_VALIDATE_BOOLEAN );
 			$options['map_verifydomain'] = filter_input( INPUT_POST, 'map_verifydomain', FILTER_VALIDATE_BOOLEAN );
-			$options['map_instructions'] = wp_kses( filter_input( INPUT_POST, 'map_instructions' ), current_filter() );
+			$options['map_instructions'] = current_user_can('unfiltered_html') ? filter_input( INPUT_POST, 'map_instructions' ) : wp_kses_post( filter_input( INPUT_POST, 'map_instructions' ) );
 
 			// update options
 			update_site_option( 'domain_mapping', $options );
