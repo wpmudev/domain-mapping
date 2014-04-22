7df0e50fa2912e6ef18b02b13cbb6090ac1b6451
sam <sam@f53fe46e-8c02-459b-9fdd-93e75771b278>
2014-04-22 19:44:16 +0000

    taking care of deprecated get_current_site_name function in sunrise
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/plugins/domain-mapping@14000 f53fe46e-8c02-459b-9fdd-93e75771b278

--- sunrise.php
+++ sunrise.php
@@ -35,7 +35,20 @@ if ( !empty( $mapped_id ) ) {
 	$current_site = $wpdb->get_row( $wpdb->prepare( "SELECT * from {$wpdb->site} WHERE id = %d LIMIT 1", $current_blog->site_id ) );
 	$current_site->blog_id = $wpdb->get_var( $wpdb->prepare( "SELECT blog_id FROM {$wpdb->blogs} WHERE domain = %s AND path = %s", $current_site->domain, $current_site->path ) );
 
-	$current_site = get_current_site_name( $current_site );
+    if( function_exists( "get_current_site_name" ) )
+        $current_site = get_current_site_name( $current_site );
+
+    //set site_name
+    if( !isset( $current_site->site_name ) || empty($current_site->site_name) ){
+        $current_site->site_name = wp_cache_get( $current_site->id . ':site_name', 'site-options' );
+        if ( ! $current_site->site_name ) {
+            $current_site->site_name = $wpdb->get_var( $wpdb->prepare( "SELECT meta_value FROM $wpdb->sitemeta WHERE site_id = %d AND meta_key = 'site_name'", $current_site->id ) );
+            if ( ! $current_site->site_name )
+                $current_site->site_name = ucfirst( $current_site->domain );
+            wp_cache_set( $current_site->id . ':site_name', $current_site->site_name, 'site-options' );
+        }
+    }
+
 	$current_blog->path = $current_site->path;
 
 	define( 'DOMAIN_MAPPING', 1 );
