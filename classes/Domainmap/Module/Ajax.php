1f9beccf67425fdd2688850fa550c54ec931ab9d
Sam Najian <samnajian@gmail.com>
2014-07-31 13:55:56 +0430

    better domain validation when adding new domain

--- classes/Domainmap/Module/Ajax.php
+++ classes/Domainmap/Module/Ajax.php
@@ -45,7 +45,9 @@ class Domainmap_Module_Ajax extends Domainmap_Module {
         $domain = Domainmap_Punycode::encode($domain);
 		return preg_match( "/^([A-Za-z0-9](-*[A-Za-z0-9])*)(\.([a-z\d](-*[a-z\d])*))*$/i", $domain ) //valid chars check
 			&& preg_match( "/^.{1,253}$/", $domain ) //overall length check
-			&& preg_match( "/^[^\.]{1,63}(\.[^\.]{2,63})+$/", $domain ); //length of each label
+			&& preg_match( "/^[^\.]{1,63}(\.[^\.]{2,63})+$/", $domain ) //length of each label
+            && gethostbyname( $domain ) === ( isset( $_SERVER["SERVER_ADDR"] ) ? $_SERVER['SERVER_ADDR'] : gethostbyname( $_SERVER['SERVER_NAME'] ) ) //if domain points to current ip
+            ;
 	}
 
 	/**
