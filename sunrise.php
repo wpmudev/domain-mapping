751f148678fb0bc3993413240c4cc280ad50e23a
Sam Najian <dev.samnajian@gmail.com>
2014-09-27 22:25:43 +0330

    fixed issue with php v5.3-

--- sunrise.php
+++ sunrise.php
@@ -1,7 +1,7 @@
 <?php
 $sunrises = array(
-	"dm_sunrise" => defined( "WP_PLUGIN_DIR" ) ?  rtrim( WP_PLUGIN_DIR, '/\\' ) . "/domain-mapping/inc/sunrise.php" : __DIR__ .  "/plugins/domain-mapping/inc/sunrise.php",
-	"md_sunrise" => defined( "WP_PLUGIN_DIR" ) ?  rtrim( WP_PLUGIN_DIR, '/\\' ) . "/multi-domains/inc/sunrise.php" : __DIR__ .  "/plugins/multi-domains/inc/sunrise.php"
+	"dm_sunrise" => defined( "WP_PLUGIN_DIR" ) ?  rtrim( WP_PLUGIN_DIR, '/\\' ) . "/domain-mapping/inc/sunrise.php" : dir( __FILE__ ) .  "/plugins/domain-mapping/inc/sunrise.php",
+	"md_sunrise" => defined( "WP_PLUGIN_DIR" ) ?  rtrim( WP_PLUGIN_DIR, '/\\' ) . "/multi-domains/inc/sunrise.php" : dir( __FILE__ ) .  "/plugins/multi-domains/inc/sunrise.php"
 );
 
 foreach( $sunrises as $sunrise ){
