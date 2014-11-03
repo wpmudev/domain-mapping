bba3b451c39ad76496520a9d530aa70781131b03
Sam Najian <dev.samnajian@gmail.com>
2014-11-03 21:38:49 +0330

    resolve bug in wrong admin-ajax.php url

--- classes/Domainmap/Module/Cdsso.php
+++ classes/Domainmap/Module/Cdsso.php
@@ -166,10 +166,9 @@ class Domainmap_Module_Cdsso extends Domainmap_Module {
 		if ( is_user_logged_in() || get_current_blog_id() == 1 || filter_input( INPUT_GET, self::ACTION_KEY ) != self::ACTION_LOGOUT_USER ) {
 			return;
 		}
-//		switch_to_blog( 1 );
-		$url = add_query_arg( 'action', self::ACTION_LOGOUT_USER, network_admin_url( 'admin-ajax.php' ) );
+
+		$url = add_query_arg( 'action', self::ACTION_LOGOUT_USER, str_replace("/network", "", network_admin_url( 'admin-ajax.php' ) ) );
 		echo '<script type="text/javascript" src="', $url, '"></script>';
-//		restore_current_blog();
 	}
 
 	/**
@@ -274,12 +273,10 @@ class Domainmap_Module_Cdsso extends Domainmap_Module {
 		echo '</script>';
 
 
-//		switch_to_blog( 1 );
 		$url = add_query_arg( array(
 			'action' => self::ACTION_PROPAGATE_USER,
 			'auth'   => wp_generate_auth_cookie( $user->ID, time() + MINUTE_IN_SECONDS ),
-		), network_admin_url( 'admin-ajax.php' ) );
-//		restore_current_blog();
+		), str_replace("/network", "", network_admin_url( 'admin-ajax.php' ) ) );
 
 		echo '<script type="text/javascript" src="', $url, '"></script>';
 	}
@@ -298,12 +295,8 @@ class Domainmap_Module_Cdsso extends Domainmap_Module {
 		if (   is_user_logged_in() ||  1  === get_current_blog_id() || filter_input( INPUT_GET, self::ACTION_KEY ) == self::ACTION_AUTHORIZE_USER ) {
 			return;
 		}
-
-//		switch_to_blog( 1 );
-		$url = add_query_arg( 'action', self::ACTION_SETUP_CDSSO, network_admin_url( 'admin-ajax.php' ) );
-
+		$url = add_query_arg( 'action', self::ACTION_SETUP_CDSSO, str_replace("/network", "", network_admin_url( 'admin-ajax.php' ) ) );
 		echo '<script type="text/javascript" src="', $url, '"></script>';
-//		restore_current_blog();
 	}
 
 	/**
