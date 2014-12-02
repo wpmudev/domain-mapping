e0caf64079a23014a320c07e695efb8a36d2c2b5
Sam Najian <dev.samnajian@gmail.com>
2014-12-02 21:06:47 +0330

    add ability to define custom sunrise file

--- sunrise.php
+++ sunrise.php
@@ -1,11 +1,11 @@
 <?php
 $sunrises = array(
-	"dm_sunrise" => defined( "WP_PLUGIN_DIR" ) ?  rtrim( WP_PLUGIN_DIR, '/\\' ) . "/domain-mapping/inc/sunrise.php" : dirname( __FILE__ ) .  "/plugins/domain-mapping/inc/sunrise.php",
-	"md_sunrise" => defined( "WP_PLUGIN_DIR" ) ?  rtrim( WP_PLUGIN_DIR, '/\\' ) . "/multi-domains/inc/sunrise.php" : dirname( __FILE__ ) .  "/plugins/multi-domains/inc/sunrise.php"
+	"dm_sunrise" => defined("DM_CUSTOM_SUNRISE") ? DM_CUSTOM_SUNRISE : ( defined( "WP_PLUGIN_DIR" ) ?  rtrim( WP_PLUGIN_DIR, '/\\' ) . "/domain-mapping/inc/sunrise.php" : dirname( __FILE__ ) .  "/plugins/domain-mapping/inc/sunrise.php" ),
+	"md_sunrise" => defined("MD_CUSTOM_SUNRISE") ? MD_CUSTOM_SUNRISE : ( defined( "WP_PLUGIN_DIR" ) ?  rtrim( WP_PLUGIN_DIR, '/\\' ) . "/multi-domains/inc/sunrise.php" : dirname( __FILE__ ) .  "/plugins/multi-domains/inc/sunrise.php" )
 );
 
 foreach( $sunrises as $sunrise ){
-  if( is_readable( $sunrise ) ){
-	include $sunrise;
-  }
+	if( is_readable( $sunrise ) ){
+		include $sunrise;
+	}
 }
