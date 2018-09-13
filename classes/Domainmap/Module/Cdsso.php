88f380274bbcf4f186873a925fbe27a6bdaefa2f
leonidasmi <leonidas.milossis@gmail.com>
2018-09-13 14:22:18 +0300

    Improve copy of redirected login

--- classes/Domainmap/Module/Cdsso.php
+++ classes/Domainmap/Module/Cdsso.php
@@ -182,7 +182,7 @@ class Domainmap_Module_Cdsso extends Domainmap_Module {
 	 */
 	public function get_login_message( $message ) {
 		return $this->_do_propagation
-				? '<p class="message">' . esc_html__( 'You have logged in successfully. You will be redirected to desired page during next 5 seconds.', 'domainmap' ) . '</p>'
+				? '<p class="message">' . esc_html__( 'You have logged in successfully. You will be redirected to the desired page during the next 5 seconds.', 'domainmap' ) . '</p>'
 				: $message;
 	}
 
